b27b0f337295de26e2ffb25b7225b90ef0ff0061
baminou <brice.aminou@gmail.com>
2019-01-31 14:13:02 -0500

    fix on analysis collision to save payload

--- libraries/sing/sing/operations/status/__init__.py
+++ libraries/sing/sing/operations/status/__init__.py
@@ -16,10 +16,11 @@ class Status(Singoperationtype):
     def _parser(self, main_parser):
         main_parser.add_argument('study', help="ICGC study ID")
         main_parser.add_argument('upload_id', help="Upload ID")
+        main_parser.add_argument('-i','--ignore-collision',dest='ignore_collision',help="Ignore analysis collision",action='store_true')
         return
 
     def _run(self):
-        response = requests.get('%s/upload/%s/status/%s' % (self.song_server,self.args.study,self.args.upload_id))
+        response = requests.get('%s/upload/%s/status/%s?ignoreAnalysisIdCollisions=%s' % (self.song_server,self.args.study,self.args.upload_id,str(self.args.ignore_collision).lower()))
         if not response.status_code == 200:
             raise Exception(response.text)
         print(json.dumps(response.json()))
